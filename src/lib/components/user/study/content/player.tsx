dfb38925e4baf7fbe9c97add2d35bf2d27a35ef0
wills <godswillijeze@gmail.com>
2024-04-16 14:48:57 +0100

    video completion

--- src/lib/components/user/study/content/player.tsx
+++ src/lib/components/user/study/content/player.tsx
@@ -1,25 +1,52 @@
 import { FC } from "react";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
+import { markSubRead } from "../../../../../services/api/programApi";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 type activeProps = {
   mediaType: string;
   media: string;
   duration: number;
+  id: string;
 };
 interface Props {
   active: activeProps;
 }
 const CoursePlayer: FC<Props> = ({ active }) => {
+  const { id } = useParams();
+  const markAction = async () => {
+    const payload = {
+      courseContentSub: active.id,
+    };
+    await markSubRead(payload, `${id}`)
+      .then((res) => {
+        toast.success(res.message);
+      })
+      .catch(() => {
+      });
+  };
   if (active.mediaType === "video") {
     return (
       <div>
         <div className="h-[380px] bg-black">
-        <ReactPlayer width={'100%'} height={'100%'} url={active?.media? active?.media : 'https://www.youtube.com/watch?v=LXb3EKWsInQ'} controls pip stopOnUnmount={false}/>
+          <ReactPlayer
+            width={"100%"}
+            height={"100%"}
+            url={
+              active?.media
+                ? active?.media
+                : "https://www.youtube.com/watch?v=LXb3EKWsInQ"
+            }
+            controls
+            pip
+            stopOnUnmount={false}
+            onEnded={() => markAction()}
+          />
         </div>
       </div>
     );
-  }
-  else if (active.mediaType === "image") {
+  } else if (active.mediaType === "image") {
     return (
       <div>
         <div className="h-[380px] bg-black">
@@ -31,15 +58,18 @@ const CoursePlayer: FC<Props> = ({ active }) => {
         </div>
       </div>
     );
-  }
-  else{
+  } else {
     return (
-        <div>
-          <div className="h-[380px] bg-blue-300 border-[4px] border-gray-400">
-            <img src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1706266960/rsh/Property_1_Variant6_1_hdn7cq.png" alt="class" className="object-cover w-full h-full" />
-          </div>
+      <div>
+        <div className="h-[380px] bg-blue-300 border-[4px] border-gray-400">
+          <img
+            src="https://res.cloudinary.com/greenmouse-tech/image/upload/v1706266960/rsh/Property_1_Variant6_1_hdn7cq.png"
+            alt="class"
+            className="object-cover w-full h-full"
+          />
         </div>
-      );
+      </div>
+    );
   }
 };
 
